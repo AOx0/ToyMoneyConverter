a2ac6b8fb65a8465d1667e9afd4d292375fc4f2b
AOx0 <50227494+AOx0@users.noreply.github.com>
2021-07-28 14:32:53 -0500

    Protect Money attributes

--- get_values.py
+++ get_values.py
@@ -5,27 +5,27 @@ import requests
 class __Money:
 
     def __init__(self):
-        self.values: {str: float} = {}
+        self.__values: {str: float} = {}
 
-        self.page: requests.Response = requests.get('https://www.iban.com/exchange-rates')
-        self.tree: html.HtmlElement = html.fromstring(self.page.content)
+        self.__page: requests.Response = requests.get('https://www.iban.com/exchange-rates')
+        self.__tree: html.HtmlElement = html.fromstring(self.__page.content)
 
         for i in range(32):
             name = str(
-                self.tree.xpath(
+                self.__tree.xpath(
                     f'/html/body/div[1]/div[2]/div/div[2]/div[1]/div/table/tbody/tr[{i + 1}]/td[1]/text()')[0]
             ).replace("\t", "").replace(" ", "")
             value = float(
-               self.tree.xpath(
+               self.__tree.xpath(
                     f"/html/body/div[1]/div[2]/div/div[2]/div[1]/div/table/tbody/tr[{i + 1}]/td[3]/strong/text()")[0]
             )
 
-            self.values[name] = value
+            self.__values[name] = value
 
-        self.value_keys = tuple(self.values.keys())
+        self.__value_keys = tuple(self.__values.keys())
 
-    def get_names(self) -> (str, str, ...): return self.value_keys
-    def get_values(self) -> {str: float}: return self.values
+    def get_names(self) -> (str, str, ...): return self.__value_keys
+    def get_values(self) -> {str: float}: return self.__values
     def reload(self) -> None: self.__init__()
 
 
