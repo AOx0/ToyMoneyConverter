eacf45d7ecc0fd64c06219dc0e0fc579f5dcb4d9
Elyayoveloz <luisfersilva2002@gmail.com>
2021-08-10 19:44:36 -0500

    Cambios listos

--- converter.py
+++ converter.py
@@ -1,9 +1,11 @@
 converter_money = {
-    'usd' = 1
-    'mxn' = 20.01
-    'euros' = 0.85
+    'usd' = 1,
+    'mxn' = 20.01,
+    'euros' = 0.85,
     'pounds' = 0.72
-    'putos' = 858585
 }
 
-def converter():
+def converter(amount, money_have, money_want, diccionario) -> float:
+    return converter_1(amount(money_have)/amount(money_want), diccionario)
+
+
